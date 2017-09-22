c9d0f2de9616ba3abdd2866f09c34be25b69a24f
Sreejit Sreedharan <sreejitsreedharan@TI9206-MOHAN.local>
2017-09-22 14:15:30 +0530

    Changed the files

--- Provider/Utility/FontFamily.cs
+++ Provider/Utility/FontFamily.cs
@@ -3,7 +3,10 @@ namespace Provider.Utility
 {
     public static class FontFamilyName
     {
-        public static readonly string Lobster = "Lobster";
+        //public static readonly string Lobster = "Lobster";
+        //public static readonly string LatoRegular = "Lato-Regular";
+
+        public static readonly string Lobster = "Calibri";
         public static readonly string LatoRegular = "Lato-Regular";
     }
 }
