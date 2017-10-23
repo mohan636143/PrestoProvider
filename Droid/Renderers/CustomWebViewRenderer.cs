45d5c73ac36542878633a4af81e7fe943cdf2233
MOHAN P V R <sreejitsreedharan@TI9206-MOHAN.local>
2017-10-23 15:51:24 +0530

    added custom webview renderers for ios and android to clear cache on loading to prevent facebook user data being cached.

---
+++ Droid/Renderers/CustomWebViewRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.Content;
+using Android.OS;
+using Android.Webkit;
+using Provider.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AndroidWebView = Android.Webkit;
+
+[assembly:ExportRenderer(typeof(Xamarin.Forms.WebView),typeof(CustomWebViewRenderer))]
+namespace Provider.Droid.Renderers
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+		{
+            try
+            {
+                base.OnElementChanged(e);
+                AndroidWebView.WebView wV = Control as AndroidWebView.WebView;
+                wV.ClearCache(true);
+                wV.ClearHistory();
+                wV.ClearFormData();
+                ClearCookies();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+		public void ClearCookies()
+		{
+
+            if (Build.VERSION.SdkInt >= Build.VERSION_CODES.Lollipop)
+			{
+                CookieManager.Instance.RemoveAllCookies(null);
+                CookieManager.Instance.Flush();
+			}
+			else
+			{
+                CookieSyncManager cookieSyncMngr = CookieSyncManager.CreateInstance(Android.App.Application.Context);
+                cookieSyncMngr.StartSync();
+                CookieManager cookieManager = CookieManager.Instance;
+				cookieManager.RemoveAllCookie();
+				cookieManager.RemoveSessionCookie();
+				cookieSyncMngr.StopSync();
+				cookieSyncMngr.Sync();
+			}
+		}
+
+    }
+}
---
+++ iOS/Renderers/CustomWebViewRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Foundation;
+using Provider.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.WebView), typeof(CustomWebViewRenderer))]
+namespace Provider.iOS.Renderers
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            try
+            {
+                base.OnElementChanged(e);
+
+
+
+                NSHttpCookieStorage storage = NSHttpCookieStorage.SharedStorage;
+                foreach (NSHttpCookie cookie in storage.Cookies)
+                {
+                    if (cookie.Domain == ".facebook.com")
+                    {
+                        storage.DeleteCookie(cookie);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
